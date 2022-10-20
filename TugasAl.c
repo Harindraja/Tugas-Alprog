cc187027baac8e058a0ba9b94ebfa90421db902a
Harindraja <harischandrajaya01@gmail.com>
2022-10-20 10:38:01 +0800

    Awalan 3

--- TugasAl.c
+++ TugasAl.c
@@ -3,15 +3,32 @@
 int main(void){
     int pilihan;
 
-    printf("|==========================|");
-    printf("|    Selamat Datang di     |");
-    printf("|  Penghitungan Tarif Air  |");
-    printf("|==========================\n\n|");
-   
-    printf("|Pilihlah Jenis Kelompok Pelanggan : |");
+    printf("|==========================|\n");
+    printf("|    Selamat Datang di     |\n");
+    printf("|  Penghitungan Tarif Air  |\n");
+    printf("|==========================|\n\n");
     
-    printf("|1. Non Niaga Bersubsidi (Rumah Tangga Memiliki daya listrik 450 VA - 900 VA)   |");
-    printf("|2. Non Niaga Tidak Bersubsidi (Rumah Tangga Memiliki daya listrik >=1300)      |");
-    printf("|3. Niaga                                                                       |");
+    printf("|1. Non Niaga Bersubsidi (Rumah Tangga Memiliki daya listrik 450 VA - 900 VA)   |\n");
+    printf("|2. Non Niaga Tidak Bersubsidi (Rumah Tangga Memiliki daya listrik >=1300)      |\n");
+    printf("|3. Niaga                                                                       |\n\n");
+
+    printf("Pilihlah Jenis Kelompok Pelanggan : ");
     scanf("%d", &pilihan);
+
+    switch(pilihan){
+        case 1:
+            NonNiagaBersubsidi();
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+    }
+    return 0;
+}
+ 
+int NonNiagaBersubsidi(){
+    printf("\n=========== Non Niaga Bersubsidi ===========\n");
+    
 }
